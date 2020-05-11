de6443eda14e367157970778c1bd1d7963e96c5b
Damien HERBERT <dherbert98@gmail.com>
2020-05-11 18:26:15 +0200

    unique user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,13 +24,13 @@ class User implements UserInterface
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\Email()
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $username;
 
